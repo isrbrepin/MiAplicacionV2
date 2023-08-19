9c79bfee1d5efcfd5ae5fdd2773856584e2d223e
isrbrepin <isrbrepin@alum.us.es>
2023-08-19 13:28:07 +0200

    En proceso del boton de ayuda

--- app/src/main/java/es/us/myapplication/HeaderActivity.kt
+++ app/src/main/java/es/us/myapplication/HeaderActivity.kt
@@ -2,10 +2,22 @@ package es.us.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class HeaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_header)
+    }
+    fun openOverlay(view: View) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val overlayFragment = HelpFragment() // Aquí asumo que tienes una instancia de HelpFragment
+        fragmentTransaction.add(R.id.overlay_container, overlayFragment)
+        fragmentTransaction.addToBackStack(null) // Agregar a la pila de fragmentos
+
+        fragmentTransaction.commit()
     }
 }
