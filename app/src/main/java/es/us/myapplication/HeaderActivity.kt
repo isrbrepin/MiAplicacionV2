f8ebb3047613c6b31d5f33937e00bd7cc0451e45
isrbrepin <isrbrepin@alum.us.es>
2023-08-21 20:22:53 +0200

    Ayuda y MainActivity terminados

--- app/src/main/java/es/us/myapplication/HeaderActivity.kt
+++ app/src/main/java/es/us/myapplication/HeaderActivity.kt
@@ -3,16 +3,21 @@ package es.us.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class HeaderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_header)
+
     }
     fun openHelpActivity(view: View) {
-        val intent = Intent(this, HelpActivity::class.java)
-        startActivity(intent)
+        // Mostrar un Toast con información de contacto
+        val contactoInfo = "Información de contacto:\nCorreo: example@example.com\nTeléfono: +123456789"
+        Toast.makeText(this, contactoInfo, Toast.LENGTH_LONG).show()
     }
 
 
--- app/src/main/java/es/us/myapplication/MainActivity.kt
+++ app/src/main/java/es/us/myapplication/MainActivity.kt
@@ -3,33 +3,19 @@ package es.us.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
-    fun openInfoActivity(view: View) {
-        val intent = Intent(this, InfoActivity::class.java)
-        startActivity(intent)
-        this@MainActivity.overridePendingTransition(
-            R.anim.left_in,
-            R.anim.left_out
-        )
-    }
-    fun openPreoperatorioActivity(view: View) {
-        val intent = Intent(this, PreoperatorioActivity::class.java)
-        startActivity(intent)
-    }
-    fun openPostoperatorioActivity(view: View) {
-        val intent = Intent(this, PostoperatorioActivity::class.java)
-        startActivity(intent)
-    }
-    fun openHelpActivity(view: View) {
-        val intent = Intent(this, HelpActivity::class.java)
-        startActivity(intent)
+        val btnComenzar: Button = findViewById(R.id.btn_comenzar)
+        // Configura el evento de clic del botón
+        btnComenzar.setOnClickListener {
+            // Crear una intención para lanzar la SecondActivity
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
